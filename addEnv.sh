42d89aa8e1c0b1cd152f36fe05a9deb17bf9295f
chenganglist <1464125055@qq.com>
2016-07-14 17:26:01 +0800

    编译helloServlet成功

--- addEnv.sh
+++ addEnv.sh
@@ -1,4 +1,5 @@
 ##/usr/lib/jvm  yum默认安装java的位置
+##通过javac查看java版本，将.jar包移动到对应目录即可
 export   PATH=/usr/local/tomcat/lib:$PATH
 export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk
 export PATH=$JAVA_HOME/bin:$PATH 
