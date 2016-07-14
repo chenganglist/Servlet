b9a94f6a433a843f522c458a4e22516463b940a1
chenganglist <1464125055@qq.com>
2016-07-14 19:52:48 +0800

    servlet返回json数据

--- WEB-INF/classes/helloServlet.java
+++ WEB-INF/classes/helloServlet.java
@@ -1,5 +1,6 @@
 // Import required java libraries
 import java.io.*;
+import java.util.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
  
@@ -23,7 +24,7 @@ public class helloServlet extends HttpServlet {
       
       // Actual logic goes here.
       PrintWriter out = response.getWriter();
-      out.println(request);
+      //out.println(request);
       out.println(message);
   }
 
@@ -31,18 +32,49 @@ public class helloServlet extends HttpServlet {
                    HttpServletResponse response)
     throws ServletException, IOException 
   {
+      //HashMap<String, String> hashMap = new HashMap<String, String>();  
       // Servlet 代码
       // Set response content type
       response.setContentType("text/html");
       
       // Actual logic goes here.
       PrintWriter out = response.getWriter();
-      out.println(request);
-      out.println(message);
+      //out.println(request);
+      //out.println(request.getParameter("username"));
+      //out.println(request.getParameter("password"));
+      Enumeration paramNames = request.getParameterNames();
+      out.println("{");
+      while(paramNames.hasMoreElements()) 
+      {
+          String paramName = (String)paramNames.nextElement();
+          out.print("paramName:");
+          String[] paramValues = request.getParameterValues(paramName);
+          // 读取单个值的数据
+          if (paramValues.length == 1) 
+          {
+                String paramValue = paramValues[0];
+                if (paramValue.length() == 0)
+                  out.println("没有值,");
+                else
+                  out.println("paramValue,");
+          }else 
+          {
+                // 读取多个值的数据
+                out.println("[");
+                for(int i=0; i < paramValues.length; i++) 
+                {
+                    out.println(paramValues[i]+",");
+                }
+                out.println("]");
+          }
+      }
+      out.println("}");
+      //out.println(message);
   }
   
   public void destroy()
   {
       // do nothing.
   }
+
 }
